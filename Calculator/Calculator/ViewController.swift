87c3eab0bd97f9bce6c60b5dd8340ad4aa46880e
soni suman <sonisuman67@gmail.com>
2020-05-04 04:20:22 +0530

    Implemented calculator UI in autolayout.

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Soni Suman on 04/05/20.
+//  Copyright © 2020 Soni Suman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
