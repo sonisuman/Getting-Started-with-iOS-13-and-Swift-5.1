fac66eadfec5d5dffeb1edfaf17b3671fda91c18
soni suman <sonisuman67@gmail.com>
2021-05-22 21:07:27 +0530

    Added textfield delegate methods.

--- Climba/Climba/Controller/WeatherViewController.swift
+++ Climba/Climba/Controller/WeatherViewController.swift
@@ -10,16 +10,45 @@ import UIKit
 class WeatherViewController: UIViewController {
     
     @IBOutlet weak var conditionImageView: UIImageView!
-    
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
-    
-
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        searchTextField.delegate = self
     }
+    
+    @IBAction func searchButtonPressed(_ sender: UIButton) {
+        
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+        
+    }
+}
 
-
+extension UIViewController: UITextFieldDelegate {
+    
+    public func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        textField.endEditing(true)
+        print(textField.text!)
+        return true
+    }
+    
+    public func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        textField.text = ""
+    }
+    
+    public func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        
+        if textField.text != ""{
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
 }
